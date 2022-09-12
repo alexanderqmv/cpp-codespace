be50b996069d9ef408420a09d8748f6fc1e25ab0
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2022-09-12 22:33:27 +0400

    added * if (d = 0) /*...*/

--- dsqrt.cpp
+++ dsqrt.cpp
@@ -25,10 +25,15 @@ int SomeScript() {
 				x1 = (-1 * b + sqrt(d) / 4 * a);
 
 				x2 = (-1 * b - sqrt(d) / 4 * a);
+				cout << "x1 - " << x1 << endl;
+				cout << "x2 - " << x2 << endl;
 			}
-			cout << "x1 - " << x1 << endl;
-			cout << "x2 - " << x2 << endl;
+			
+				else if (d = 0) {
+					x1 = (-1 * b / 2 * a);
 
+					cout << "x1 - " << x1;
+				}
 
 		return d;
 
