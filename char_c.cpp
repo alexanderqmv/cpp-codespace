475054ef46b8bfd83f72cdc80a855d5bd2f1d279
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2022-10-08 16:06:40 +0400

    Create char_c.cpp

---
+++ char_c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <experimental/iterator>
+
+
+int main(int argc, char const *argv[])
+{
+	
+	const char i[] = "QWERTYUIOPADDDDDDDDDDDDDDDDDDDDDDDDDDDDDD";
+	std::cout << i << " | " <<  sizeof(i);
+	return 0;
+}
