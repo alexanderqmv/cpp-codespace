68ed6539db82f606997267cbaab21a9e9de92a99
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2022-10-09 12:15:04 +0400

    Create include.h

---
+++ headers/include.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class MAIN {
+	public:
+		MAIN(int n1, int n2);
+		int Sum();
+	private:
+		int val1;
+		int val2;
+};
