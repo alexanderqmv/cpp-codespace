4da997ebd6878abe1146dd1057e531b8218009e4
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2022-10-13 20:58:44 +0400

    Create include.h

---
+++ headers/arrray/include.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <sha2>
+#include <ctime>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+
+ 
