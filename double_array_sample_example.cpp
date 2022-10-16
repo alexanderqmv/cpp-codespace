f9ffaa7e6dee744b977d590e6ffce59b7283f57b
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2022-10-16 20:30:38 +0400

    Update double_array_sample_example.cpp

--- double_array_sample_example.cpp
+++ double_array_sample_example.cpp
@@ -9,9 +9,9 @@ int main(int argc, char const *argv[])
 	
 	const int ROW = 2;
 	const int COL = 4;
-					//  0        1
+			//  0        1
 	int arr[ROW][COL]{{2,4}, {5,3,9,7}};
-					// 0,1    0 1 2 3
+			// 0,1    0 1 2 3
 	std::cout << arr[1][3] << std::endl; 
 
 	std::cout << sizeof(arr);
