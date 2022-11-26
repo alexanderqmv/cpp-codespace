0520ac7075776bf12e2d2954935da5303ebf4fe5
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2022-11-26 14:59:52 +0400

    Update homework_d.cpp

--- tests/homework_d.cpp
+++ tests/homework_d.cpp
@@ -2,19 +2,34 @@
 
 using namespace std;
 
+double KilogramsInPounds(int kilograms)
+{
+
+    return (kilograms *  2.20462);
+}
+
 double PoundsInKilograms(int pounds)
 {
 
-    return pounds * 0.453592;
+    return (pounds * 0.453592);
 
 }
 
 int main()
 {   
     int pounds;
+    cout << "[CONVERT] Pounds in kilograms > ";
     cin >> pounds;
 
     cout << PoundsInKilograms(pounds);
+    cout << endl;
 
+    int kilograms; 
+    cout << "[CONVERT] Kilograms in pounds > "; 
+    cin >> kilograms;
+
+    cout << KilogramsInPounds(kilograms);
+
+    cout << endl;
     return 0;
 }
