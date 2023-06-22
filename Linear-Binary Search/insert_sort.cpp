591b2b5ea51ec1cd8a0d543098a6911f46645338
Alexander <112755279+alexanderqmv@users.noreply.github.com>
2023-06-22 13:02:12 +0400

    bbAyEr v0.01

--- Linear-Binary Search/insert_sort.cpp
+++ Linear-Binary Search/insert_sort.cpp
@@ -3,30 +3,24 @@
 using namespace std;
 
 
+
 int main()
 {
-
     int i,j;
-
     const int N = 10;
     int a[N] = {12, 5, 9 ,4 , 11, 21, 3, 14,18, 20};
-    
     int buff = 0;
-
     for (i = 1; i < N; ++i)
     {
-
         buff = a[i]; 
-
 
         for (j = i - 1; j >= 0 and a[j] > buff; j--)
         
             a[j + 1] = a[j];
             a[j + 1] = buff;     
-
-
     }
 
+ 
     for (int i = 0; i < N; ++i)
     {
         cout << a[i] << " ";
